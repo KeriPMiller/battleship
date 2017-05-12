afe239637c1df6906d6968049a4ceef8365b38dc
KeriPMiller <KeriPMiller@users.noreply.github.com>
2017-05-12 17:12:57 -0400

    added function to generate random ship locations

--- battleshipJs.js
+++ battleshipJs.js
@@ -19,11 +19,56 @@ var model = {
   boardSize: 7,
   numShips: 3,
   shipLength: 3,
-  shipsSunk: 0,
+  ships: [{ locations: [0, 0, 0], hits:["", "", ""]},
+          { locations: [0, 0, 0], hits:["", "", ""]},
+          { locations: [0, 0, 0], hits:["", "", ""]}],
 
-  ships: [{ locations: ["06", "16", "26"], hits:["", "", ""]},
-          { locations: ["24", "34", "44"], hits:["", "", ""]},
-          { locations: ["10", "11", "12"], hits:["", "", ""]}],
+  generateShipLocations: function() {
+    var locations;
+    for( var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+    }
+  },
+
+  generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+
+    if (direction === 1) {
+      // Generate a starting location for a horizontal ship
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shiplength));
+    } else {
+      // Generate a starting location for a vertical ship
+      row = Math.floor(Math.random() * (this.boardSize - this.shiplength));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+    var newShipLocations = [];
+    for (var i = 0; i < this.shiplength; i++) {
+      if (direction === 1) {
+        // add ship location for horizontal ship
+            newShipLocations.push(row + "" + (col + i));
+        } else {
+        // add ship location for vertical ship
+            newShipLocations.push((row + i) + "" + col);
+        }
+    }
+    return newShipLocations;
+  },
+
+  collision: function(locations) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = model.ships[i];
+      for (var j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
 
   fire: function(guess){
     for (var i = 0; i < this.numShips; i++){
@@ -97,6 +142,8 @@ function init() {
   fireButton.onclick = handleFireButton;
   var guessInput = document.getElementById("guessInput");
   guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
 }
 
 function handleFireButton() {
